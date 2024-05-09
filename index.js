fed006508ea6213904799eaafa4f62aeca7a6d98
olamide <olamideashaolu318.gmail.com>
2024-05-09 18:08:11 +0100

    commit new codes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+//hamburgers variables
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
+//hamurger menu events
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
 
+// Closing the navbar with
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
